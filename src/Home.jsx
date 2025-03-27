e694e03b20a9354794dcf897a2acf30e08cf2eb5
GYANESH KUMAR <Gyanesh5009@gmail.com>
2025-03-27 20:34:23 +0530

    fix 1

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import Category from './components/Category'
 import MostSearchCar from './components/MostSearchCar'
-import InfoSection from './components/infoSection'
+import InfoSection from './components/InfoSection'
 import Footer from './components/Footer'
 
 const Home = () => {
